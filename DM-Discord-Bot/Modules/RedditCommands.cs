254634e4513a12831da0325394b4d40b2eb0022e
Kacper Martela <kacper99@hotmail.co.uk>
2018-03-20 22:31:12 +0000

    Created RedditHandler class and added command

---
+++ DM-Discord-Bot/Modules/RedditCommands.cs
@@ -0,0 +1,46 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Threading.Tasks;
+using RedditSharp;
+using System.Collections.Generic;
+using RedditSharp.Things;
+
+namespace DM_Discord_Bot.Modules
+{
+    [Group("reddit")]
+    class RedditCommands : ModuleBase<SocketCommandContext>
+    {
+        RedditHandler redditHandler = new RedditHandler();
+
+        /*
+        [Command]
+        public async Task DefaultCommandAsync()
+        {
+
+        }
+
+        [Command("help")]
+        public async Task HelpCommandAsync()
+        {
+
+        }
+        */
+
+        [Command("top")]
+        public async Task TopPostsAsync(string subreddit)
+        {
+            List<Post> posts = redditHandler.GetTopPosts(subreddit);
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.WithTitle("Top posts from " + subreddit)
+                .WithColor(Color.Orange);
+
+            foreach(Post post in posts)
+            {
+                builder.AddInlineField(post.Title, post.Url);
+            }
+
+            await ReplyAsync("", false, builder);
+        }
+    }
+}
---
+++ DM-Discord-Bot/RedditHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RedditSharp;
+using System.IO;
+using RedditSharp.Things;
+
+namespace DM_Discord_Bot
+{
+    class RedditHandler
+    {
+        WebAgent agent;
+        Reddit reddit = new Reddit();
+
+        public RedditHandler()
+        {
+            //Read details from a file
+            StreamReader reader = new StreamReader(@"C:\Users\Kacper\Desktop\info.txt");
+            string name = reader.ReadLine();
+            string password = reader.ReadLine();
+            string clientID = reader.ReadLine();
+            string clientSecret = reader.ReadLine();
+            string redirectURL = reader.ReadLine();
+            reader.Close();
+
+            agent = new BotWebAgent(name, password, clientID, clientSecret, redirectURL);
+        }
+
+        public List<Post> GetTopPosts(string targetSubreddit)
+        {
+            List<Post> posts = new List<Post>();
+            Subreddit subreddit = reddit.GetSubreddit("/r/" + targetSubreddit);
+            foreach(var post in subreddit.Hot.Take(6))
+            {
+                if (!post.IsStickied)
+                    posts.Add(post);
+            }
+            return posts; //TODO: Change this
+        }
+    }
+}
